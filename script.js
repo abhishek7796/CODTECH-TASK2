e04017c7099b67b3b71711a08bb1ab9dc4707027
ABHISHEK.Y.SHARMA <120003948+abhishek7796@users.noreply.github.com>
2024-07-02 21:32:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const trackItemForm = document.querySelector('.track-item form');
+const orderItemForm = document.querySelector('.order-item form');
+const approveItemForm = document.querySelector('.approve-item form');
+const cancelItemForm = document.querySelector('.cancel-item form');
+
+trackItemForm.addEventListener('submit', (e)
